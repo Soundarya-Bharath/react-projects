b9be7a002b53f78e55281542efb88af6018ebe8b
Soundarya-Bharath <111206332+Soundarya-Bharath@users.noreply.github.com>
2024-03-11 19:06:09 -0300

    Add files via upload

---
+++ react-translate/src/components/languages.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Languages({ language, onLanguageChange }) {
+  return (
+    <div>
+      <label className="label">Select Language</label>
+      <div className="opts">
+        {LANGUAGES.map(({ label, value }) => {
+          return (
+            <div
+              key={label}
+              className={`opt ${language === value ? "selected" : ""}`}
+              onClick={() => onLanguageChange(value)}
+            >
+              {label}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+const LANGUAGES = [
+  { label: "Afrikaans", value: "af" },
+  { label: "Arabic", value: "ar" },
+  { label: "French", value: "fr" },
+  { label: "Hindi", value: "hi" },
+  { label: "Tamil", value: "ta" },
+  { label: "Japanese", value: "ja" },
+  { label: "Portuguese", value: "pt" },
+  { label: "Russian", value: "ru" },
+  { label: "Simplified Chinese", value: "zh-CN" },
+  { label: "Spanish", value: "es" },
+  { label: "Swahili", value: "sw" },
+  { label: "Thai", value: "th" },
+  { label: "Vietnamese", value: "vi" },
+  { label: "Turkish", value: "tr" },
+  { label: "Ukrainian", value: "uk" },
+  { label: "Urdu", value: "ur" },
+  { label: "Korean", value: "ko" },
+  { label: "Polish", value: "pl" },
+  { label: "German", value: "de" },
+  { label: "Indonesian", value: "id" },
+];
+
+export default Languages;
